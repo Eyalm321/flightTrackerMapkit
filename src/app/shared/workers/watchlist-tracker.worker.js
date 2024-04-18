8ad7e73188f10f6d1ec9f6d8afb9eb383491b5d0
Eyal <emizrachi321@gmail.com>
2024-04-18 11:25:35 -0500

    .

--- src/app/shared/workers/watchlist-tracker.worker.js
+++ src/app/shared/workers/watchlist-tracker.worker.js
@@ -63,7 +63,7 @@ async function fetchFlightData(id) {
 
 async function processFlightStatus(id, currentStatus, currentAltitude, callsign) {
     console.log(`Data retrieved for flight ${id}: Altitude: ${currentAltitude}`);
-    let storedStatus = await CapacitorKV.get(`flight_${id}`).value;
+    let storedStatus = await CapacitorKV.get(`flight_${id}`).value.trim().replace(/^"|"$/g, '');
     console.log(`Stored status for flight ${id}: ${storedStatus}`);
     console.log(`Current status for flight ${id}: ${currentStatus}`);
     storedStatus = storedStatus || 'Unknown';  // Default to 'Unknown' if no value is stored
